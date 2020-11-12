e862734ae0517f54ac051e62fc880bd7c2533632
Omar Medrano <omar.medrano.ms@gmail.com>
2020-11-11 21:02:08 -0600

    Uso de ngFor básico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  number = '1';
+
+  // Array de datos utilizado para el ejemplo de ngFor
+  items = ['Elon', 'Tyrell', 'Eliot'];
+
+  // Método para agregar un nuevo nombre a la lista items
+  addItem() {
+    this.items.push('Nuevo nombre');
+  }
+
+  // Se define una función para eliminar un nombre de la lista,
+  // tipando el parámetro de nombre index como tipo number
+  deleteItem(index: number) {
+
+    console.log('Se borró ' + index);
+
+    this.items.splice(index, 1);
+
+  }
+
 }
