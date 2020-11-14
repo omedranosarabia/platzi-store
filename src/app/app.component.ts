aad0e925f9237e8db518575ee886da1e49f9bfd0
Omar Medrano <omar.medrano.ms@gmail.com>
2020-11-13 21:41:58 -0600

    Uso de ngFor con arrays más avanzados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { Proudct } from './product.model';
+// Importación del modelo 'Product'
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,49 @@ export class AppComponent {
 
   // Array de datos utilizado para el ejemplo de ngFor
   items = ['Elon', 'Tyrell', 'Eliot'];
+
+
+  // Array de productos
+
+  // Las iteraciones sólo pueden ser hechas sobre arrays
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/1.jpg',
+      title: 'Playera 1',
+      price: 250,
+      description: 'Esta es la playera número 1'
+    },
+    {
+      id: '2',
+      image: 'assets/images/2.jpg',
+      title: 'Playera 2',
+      price: 350,
+      description: 'Esta es la playera número 2'
+    },
+    {
+      id: '3',
+      image: 'assets/images/3.jpg',
+      title: 'Playera 3',
+      price: 230,
+      description: 'Esta es la playera número 3'
+    },
+    {
+      id: '4',
+      image: 'assets/images/4.jpg',
+      title: 'Playera 4',
+      price: 235,
+      description: 'Esta es la playera número 4'
+    },
+    {
+      id: '5',
+      image: 'assets/images/5.jpg',
+      title: 'Playera 5',
+      price: 320,
+      description: 'Esta es la playera número 5'
+    }
+
+  ];
 
   // Método para agregar un nuevo nombre a la lista items
   addItem() {
--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -1,9 +1,16 @@
-export interface Proudct {
+// Modelo que utiliza una interfaz para modelar los atributos de un producto
+// De esta forma se pueden 'tipar' los productos (que las variables que necesitemos sean de tipo 'producto')
+
+// La palabra clave 'export' permite que este elemento pueda ser utilizado por
+// otros archivos
+
+// Se coloca el atributo seguido de su tipo de dato
+export interface Product {
 
     id: string;
     title: string;
     price: number;
     description: string;
     image: string;
-}
 
+}
