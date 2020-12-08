9da8590846c094b5747c718d35da7d4b622eed37
Omar Medrano <omar.medrano.ms@gmail.com>
2020-12-07 22:14:13 -0600

    Aprendizaje sobre Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,22 +1,30 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+// Los decoradores ofrecen un contexto a los artefactos de Angular
+// Los decoradores definen el rol que va a cumplir la clase
+// Para este caso se importa el "Component" debido a que esta clase es un componente
+import { Component } from '@angular/core';
 
-import { Proudct } from '../product.model';
+// Importación del modelo 'Product'
+import { Product } from '../product.model';
 
+// Metadata del componente
 @Component({
 
     selector: 'app-product',
     templateUrl: './product.component.html'
 
 })
+
+
+// Se define la clase y se coloca la palabra "export" para que esta pueda ser
+// utilizada desde cualquier parte de la aplicación
 export class ProductComponent {
 
-    // El componenete recibe una propiedad desde otro componente
-    @Input() product: Proudct;
-    @Output() productClicked: EventEmitter<any> = new EventEmitter();
-
-    addCart() {
-        console.log('Añadir al carrito');
-        this.productClicked.emit(this.product.id);
+    product: Product = {
+        id: '1',
+        image: 'assets/images/1.jpg',
+        title: 'Playera 1',
+        price: 250,
+        description: 'Esta es la playera número 1'
     }
 
 }
