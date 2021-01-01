210612e47c825a9a738a1aed907093c58365e41c
Omar Medrano <omar.medrano.ms@gmail.com>
2021-01-01 16:03:17 -0600

    Uso de routerLink y routerActive :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { DemoComponent } from './demo/demo.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // Array de declaración de Angular
 @NgModule({
@@ -32,7 +33,8 @@ import { BannerComponent } from './banner/banner.component';
     DemoComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
